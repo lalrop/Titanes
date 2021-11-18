aa05eb2f2dba4650dc01248729e3cde08fddf9ce
lalrop <lalrop@hotmail.com>
2021-11-18 12:19:56 -0300

    se agrego un toast con descripcion breve de cada distrito

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,8 @@
 class Character < ApplicationRecord
     belongs_to :user
     mount_uploaders :avatar, AvatarUploader
+
+    validates :potencialidad, inclusion:{in:0..200}
+    validates :frenesis, inclusion:{in:0..200}
+    
 end
