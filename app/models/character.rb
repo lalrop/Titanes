8083481d7d1f3d4b8e485b1dbb1f84c3b91ce2cc
lalrop <lalrop@hotmail.com>
2021-11-22 14:01:51 -0300

    se integraron los acordeones y son funcionales. Pero ojo con el reload, averiguar si este tiene algun timing o similar...Falta tema de tecnicas, armas, tinieblas, etc. Tambien la posibilidad de subir una imagen para el personaje

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
     belongs_to :user
-    mount_uploaders :avatar, AvatarUploader
+
 
     validates :potencialidad, inclusion:{in:0..200}
     validates :frenesis, inclusion:{in:0..200}
