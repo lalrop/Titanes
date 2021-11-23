3cc8817ce1e3429a89e4f723f32f9d2a628a2cac
lalrop <lalrop@hotmail.com>
2021-11-23 11:27:58 -0300

    descomentamos el modelo Distrito y Raza en el seed para ver si es problema del error en heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Distrito.create([
-#     {distrito: 'Kaiser', descripcion: ''},
-#     {distrito: 'Drake', descripcion: ''},
-#     {distrito: 'Ignea Mortis', descripcion: ''},
-#     {distrito: 'Tenjou', descripcion: ''},
-#     {distrito: 'Samsara', descripcion: ''},
-#     {distrito: 'Stahl', descripcion: ''},
-#     {distrito: 'Khronos', descripcion: ''},
-#     {distrito: 'Woohan', descripcion: ''}
-# ])
+Distrito.create([
+    {distrito: 'Kaiser', descripcion: ''},
+    {distrito: 'Drake', descripcion: ''},
+    {distrito: 'Ignea Mortis', descripcion: ''},
+    {distrito: 'Tenjou', descripcion: ''},
+    {distrito: 'Samsara', descripcion: ''},
+    {distrito: 'Stahl', descripcion: ''},
+    {distrito: 'Khronos', descripcion: ''},
+    {distrito: 'Woohan', descripcion: ''}
+])
 
-# Raza.create([
-#     {raza:'Humano' , descripcion: ''},
-#     {raza:'Gimpisto' , descripcion: ''},
-#     {raza:'Destello' , descripcion: ''},
-#     {raza:'Chechenia' , descripcion: ''},
-#     {raza:'Gag' , descripcion: ''},
-#     {raza:'Fanderei' , descripcion: ''},
-#     {raza:'Andorak' , descripcion: ''},
-#     {raza:'Winvhel' , descripcion: ''},
-#     {raza:'Chadack' , descripcion: ''},
-#     {raza:'Ritulillo' , descripcion: ''}
-# ])
+Raza.create([
+    {raza:'Humano' , descripcion: ''},
+    {raza:'Gimpisto' , descripcion: ''},
+    {raza:'Destello' , descripcion: ''},
+    {raza:'Chechenia' , descripcion: ''},
+    {raza:'Gag' , descripcion: ''},
+    {raza:'Fanderei' , descripcion: ''},
+    {raza:'Andorak' , descripcion: ''},
+    {raza:'Winvhel' , descripcion: ''},
+    {raza:'Chadack' , descripcion: ''},
+    {raza:'Ritulillo' , descripcion: ''}
+])
