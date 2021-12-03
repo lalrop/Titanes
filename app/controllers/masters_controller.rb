ba35ea1a7cd07e20136bd7d2a6fbfca9901b7a7c
lalrop <lalrop@hotmail.com>
2021-12-02 21:24:53 -0300

    lista vista y modelos de Armas, falta corregir tema de margenes, y seguir con tecnicas, experiencia y demases"

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -17,6 +17,19 @@ class MastersController < ApplicationController
     end
     
     def armas
+        @armas = Arma.all
+        @sencillas = @armas.select{|arma| arma.tipo == 'Sencilla'}
+        @marciales = @armas.select{|arma| arma.tipo == 'Marcial'}
+        @exoticas = @armas.select{|arma| arma.tipo == 'Exotica'}
+        @pistolas = @armas.select{|arma| arma.tipo == 'Pistola'}
+        @revolveres = @armas.select{|arma| arma.tipo == 'Revolver'}
+        @subfusiles = @armas.select{|arma| arma.tipo == 'Subfusil'}
+        @ametralladorasLigeras = @armas.select{|arma| arma.tipo == 'Ametralladora ligera'}
+        @rifles = @armas.select{|arma| arma.tipo == 'Rifle'}
+        @fusilesAsalto = @armas.select{|arma| arma.tipo == 'Fusil de Asalto'}
+        @escopetas = @armas.select{|arma| arma.tipo == 'Escopeta'}
+        @armasPesadas = @armas.select{|arma| arma.tipo == 'Arma pesada'}
+        @armaduras = @armas.select{|arma| arma.tipo == 'Armadura'}
 
     end
     
