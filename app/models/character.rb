8e1872493c1eb559cfecb97c1cf8b3ed9d8dc077
lalrop <lalrop@hotmail.com>
2021-12-06 10:14:31 -0300

    se agrega requisito de llenado a Potencialidad al momento de crear

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,7 @@
 class Character < ApplicationRecord
     belongs_to :user
 
-
-    validates :potencialidad, inclusion:{in:0..200}
+    validates :potencialidad, presence: true,  inclusion:{in:0..200}
     validates :frenesis, inclusion:{in:0..200}
     
 end
