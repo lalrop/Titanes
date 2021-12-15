83636b6af67ab9c911f7b8ab2d8a449daea72af2
lalrop <lalrop@hotmail.com>
2021-12-15 20:25:26 -0300

    se borra columa armas en Character

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_123234) do
+ActiveRecord::Schema.define(version: 2021_12_15_232347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_123234) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "avatar"
-    t.string "armas"
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
